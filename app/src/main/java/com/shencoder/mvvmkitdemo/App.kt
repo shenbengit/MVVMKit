cc91dfb258645f14f7668f07e9e538dcd2518500
shenben <714081644@qq.com>
2022-09-02 16:19:52 +0800

    update demo

--- app/src/main/java/com/shencoder/mvvmkitdemo/App.kt
+++ app/src/main/java/com/shencoder/mvvmkitdemo/App.kt
@@ -1,6 +1,10 @@
 package com.shencoder.mvvmkitdemo
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import com.shencoder.mvvmkit.coil.BufferedSourceFetcher
+import com.shencoder.mvvmkit.coil.ByteArrayFetcher
 import com.shencoder.mvvmkit.ext.globalInit
 import org.koin.android.java.KoinAndroidApplication
 import org.koin.core.logger.Level
@@ -11,7 +15,7 @@ import org.koin.core.logger.Level
  * @date    2021/6/30 14:18
  * @email   714081644@qq.com
  */
-class App : Application() {
+class App : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
@@ -23,5 +27,16 @@ class App : Application() {
                 )
                 .modules()
         globalInit(BuildConfig.DEBUG, "", koinApplication)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .componentRegistry {
+                //Coil 图片加载框架不支持 ByteArray ，需要自己实现
+                add(ByteArrayFetcher())
+                //直接使用网络流显示图片
+                add(BufferedSourceFetcher())
+            }
+            .build()
     }
 }
--- app/src/main/java/com/shencoder/mvvmkitdemo/MainActivity.kt
+++ app/src/main/java/com/shencoder/mvvmkitdemo/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.shencoder.mvvmkitdemo
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import com.shencoder.mvvmkit.base.view.BaseSupportActivity
 import com.shencoder.mvvmkit.base.viewmodel.DefaultViewModel
-import com.shencoder.mvvmkit.util.base64ToByteArray
-import com.shencoder.mvvmkit.util.toBase64
 import com.shencoder.mvvmkitdemo.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class MainActivity : BaseSupportActivity<DefaultViewModel,ActivityMainBinding>() {
+class MainActivity : BaseSupportActivity<DefaultViewModel, ActivityMainBinding>() {
     override fun getLayoutId(): Int {
         return R.layout.activity_main
     }
@@ -24,10 +21,10 @@ class MainActivity : BaseSupportActivity<DefaultViewModel,ActivityMainBinding>()
     }
 
     override fun initView() {
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_test_bg)
-        val base64 = bitmap.toBase64()
-        val bytes = base64.base64ToByteArray()
-//        mBinding.iv.load(bytes){
+//        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_test_bg)
+//        val base64 = bitmap.toBase64()
+//        val bytes = base64.base64ToByteArray()
+//        mBinding.iv.loadAny(bytes) {
 //
 //        }
     }
